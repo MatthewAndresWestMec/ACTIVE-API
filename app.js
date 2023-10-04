cad5a7f21b7bf5c982b7827d99d31920806ff9cb
dakotarobot213 <110577432+dakotarobot213@users.noreply.github.com>
2023-10-04 14:09:56 -0700

    Added basic code for app.js;

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('works');
+// Importing packages
+const express = require("express");
+const morgan = require("morgan");
+// Creating app
+const app = express();
+// Declaring port
+const PORT = 5000;
+
+// Static assets
+app.use(express.static("./public"));
+// Parse Form Data
+app.use(express.urlencoded({ extended: false }));
+// Parse JSON Data
+app.use(morgan("tiny"));
+app.use(express.json());
+
+// Server Listen
+const initServer = async () => {
+	try {
+		app.listen(PORT, console.log(`Server listening on port ${PORT}`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+initServer();
